195ae5e61b3be0b0b2314671c08f02c135d10042
shree <shree@DESKTOP-65S155A>
2022-09-11 21:04:23 +0530

    [Atul]-created code of abc

---
+++ src/jayhanuman/First1.java
@@ -0,0 +1,11 @@
+package jayhanuman;
+
+public class First1
+{
+public static void main(String[] args) 
+{
+	System.out.println("JAY HANUMAN");
+	System.out.println("JAY JYOTIBA");
+	
+}
+}
---
+++ src/variable/Program.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+    	String studentName;
+    	String studentDept;
+    	int studentMobileNum;
+    	char studentGrade;
+    	float studentPer;
+    	
+    	studentName = "Atul";
+    	studentDept = "Mechanical";
+    	studentMobileNum = 98227925;
+    	studentGrade = 'A';
+    	studentPer = 70.47f;
+    	
+    	System.out.println(studentName);
+        System.out.println(studentDept);
+        System.out.println(studentMobileNum);
+        System.out.println(studentGrade);
+        System.out.println(studentPer);
+}
+}
